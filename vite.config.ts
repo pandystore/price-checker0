2357f5debded03bb13bbf793832fd5711087b2cf
pandystore <pandystore1591@gmail.com>
2026-07-24 21:15:58 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig({
+  base: "/price-checker0/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
